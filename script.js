31d998d659afff53db67630a6c517406fafd2787
amgillenwater <amgillenwater@gmail.com>
2020-09-18 14:55:24 -0400

    TODO: add API req

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('Hello world')
+
+const searchButton = document.querySelector('#searchButton')
+const input = document.querySelector('#searchValue')
+
+searchButton.onclick = function (e) {
+  e.preventDefault()
+  //   console.log('onclick is working')
+  const inputValue = input.value
+//   console.log('input:', inputValue)
+}
